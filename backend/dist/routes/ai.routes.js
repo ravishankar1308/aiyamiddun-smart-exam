57e611e80e8b294f454003f0137fc0c930b9f19a
ravishankar.0813 <ravishankar.0813@gmail.com>
2025-11-28 17:09:35 +0000

    sd

--- backend/dist/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var ai_controller_1 = require("../controllers/ai.controller");
-var router = (0, express_1.Router)();
-router.post('/generate-question', ai_controller_1.generateQuestion);
-exports.default = router;
-//# sourceMappingURL=ai.routes.js.map
--- backend/dist/routes/metadata.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var metadata_controller_1 = require("../controllers/metadata.controller");
-var router = (0, express_1.Router)();
-router.get('/', metadata_controller_1.getMetadata);
-exports.default = router;
-//# sourceMappingURL=metadata.routes.js.map
